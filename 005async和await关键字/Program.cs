2c058cc9e9326d667c4ea34db726f9b3fda5ea83
shanzm <2351144690@qq.com>
2020-02-07 00:29:38 +0800

    2020年2月7日 00:28:57- Create：005async和await关键字

---
+++ 005async和await关键字/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005async和await关键字
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FirstAsyncWithTReturn();
+
+            //FirstAsyncWithTaskReturn();
+        }
+
+
+        //接收异步操作的返回值Task<T>
+        private static void FirstAsyncWithTReturn()
+        {
+            Task<int> result = SumAsync(1, 2);
+            result.ContinueWith(t => Console.WriteLine("异步操作结果为：" + result.Result));
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+
+        //不需要对返回结果操作，则可以接收Task类型的返回值，用于查看异步操作的状态等
+        private static void FirstAsyncWithTaskReturn()
+        {
+            Task result = SumAsync(1, 2);
+            result.ContinueWith(t => Console.WriteLine(result.Status));
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+
+
+        private static async Task<int> SumAsync(int num1, int num2)
+        {
+            int sum = await Task.Run(() => GetSum(num1, num2));
+            //注意因为Run()的参数是无参委托，所以单独定义GetSum()
+            return sum;
+        }
+        private static int GetSum(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
