1d56aaa54326731aa66c3933a1d6c8a6631b20b6
shanzm <2351144690@qq.com>
2020-02-11 17:18:30 +0800

    2020年2月11日 17:17:14- Update：使用BeginInvoke的最后一个参数

--- 006委托异步编程/Program.cs
+++ 006委托异步编程/Program.cs
@@ -18,7 +18,9 @@ namespace _006委托异步编程
         {
             //AddSync();
             //AddAsync();
-            AddAsyncWithCallBack();
+            //AddAsyncWithCallBack();
+            AddAsyncWithCallBack2();
+
         }
 
         //说明：使用.Invoke()执行委托调用的函数（该函数称之为引用函数），这种方式是同步的(也就是step by step）
@@ -44,7 +46,7 @@ namespace _006委托异步编程
 
 
         //说明：使用BeginInvoke()和EndInvoke()实现异步委托
-        //首先使用BeginInvoke()调用方法，返回值是IAsyncResult类型的对象，通过该对象可以查看异步操作的状态
+        //首先使用BeginInvoke()调用方法，返回值是IAsyncResult接口引用（其内部是一个AsyncResult对象）
 
         //BeginInvoke()需要做的全部事情就是缓存调用引用函数后返回的IAsyncResult类型对象，
         //并在准备获取引用函数调用的结果时，把它传给EndInvoke（）
@@ -59,7 +61,7 @@ namespace _006委托异步编程
                 return num1 + num2;
             };
             Console.WriteLine($"正在执行主线程，线程ID：{Thread.CurrentThread.ManagedThreadId}:DoSomethingBeforeInvoke");
-            IAsyncResult result = operateAdd.BeginInvoke(1, 2, null, null);//前面的参数是委托调用的方法的参数，此处最后两个参数必须是System.AsyncCallback和System.Object类型的对象，暂时按下不表，看下面说明
+            IAsyncResult result = operateAdd.BeginInvoke(1, 2,null,null);//前面的参数是委托调用的方法的参数，此处最后两个参数必须是System.AsyncCallback和System.Object类型的对象，暂时按下不表，看下面说明
             //这里使用IAsyncResult类型对象的IsCompleted属性，用于判断是否完成BeginInvoke()
             //while (!result.IsCompleted)
             //{
@@ -80,7 +82,7 @@ namespace _006委托异步编程
         }
 
 
-        //使用回调模式，回调函数会在异步方法结束后执行
+        //说明：使用回调模式，回调方法会在异步委托调用的方法结束后执行
         //原始线程中你该干什么就干什么，当异步方法结束后，回调函数会自动去处理
         private static void AddAsyncWithCallBack()
         {
@@ -91,13 +93,18 @@ namespace _006委托异步编程
              };
             Console.WriteLine($"当前执行的线程，线程ID:{Thread.CurrentThread.ManagedThreadId}:DoSomethingBeforeAsync...");
 
-            IAsyncResult iar = operateAdd.BeginInvoke(1, 2, addCallBack, null);
+            IAsyncResult iar = operateAdd.BeginInvoke(1, 2, new AsyncCallback(addCallBack), null);
+            //IAsyncResult iar = operateAdd.BeginInvoke(1, 2, addCallBack, null);
+            //因为倒数第二个参数是一个AsyncCallBack委托类型的对象，所以写作：new AsyncCallBack(addCallBack）
+            //或是在之前声明一个AsyncCallBack类型的委托对象，然后用这个对象做参数
+            //亦可以直接用回调方法做参数,写作：addCallBack
+            //其实这里只是为了方便理解，回调方法可以在这里只直接定义为一个(IAsyncRequest iar)=>{……}
             //异步方法的返回值在回调方法中已经处理，所以其实这里的返回值iar没有使用
 
             for (int i = 0; i < 6; i++)
             {
                 Thread.Sleep(1000);
-                Console.WriteLine($"当前执行的线程，线程ID:{Thread.CurrentThread.ManagedThreadId}:...");
+                Console.WriteLine($"当前执行主线程，线程ID:{Thread.CurrentThread.ManagedThreadId}:...");
             }
 
             Console.ReadKey();
@@ -108,7 +115,41 @@ namespace _006委托异步编程
         {
             AsyncResult ar = (AsyncResult)iar;//注意BeginInvoke(）返回的IAsyncResult接口引用，其中内部对象是AsyncResult类型对象
             int result = ((Func<int, int, int>)ar.AsyncDelegate).EndInvoke(iar);//获取调用BeginInvoke 的异步委托委托（这里就是operateAdd），并执行EndInvoke()
-            Console.WriteLine($"当前执行的线程，线程ID:{Thread.CurrentThread.ManagedThreadId}:异步操作的结果{result}");
+            Console.WriteLine($"当前执行的新线程，线程ID:{Thread.CurrentThread.ManagedThreadId}:异步操作的结果{result}");
         }
+
+
+        //说明:使用BeginInvoke()的最后一个参数，传入回调方法
+        //注意这个参数是Object类型，所以可以传入任何类型的数据，
+        //在回调方法中使用IAsyncResult对象的AsyncState属性获取，注意获取到的是object类型的数据，需要强转为真实类型
+        private static void AddAsyncWithCallBack2()
+        {
+            Func<int, int, int> operateAdd = (int num1, int num2) =>
+            {
+                Thread.Sleep(3000);
+                return num1 + num2;
+            };
+            Console.WriteLine($"当前执行的线程，线程ID:{Thread.CurrentThread.ManagedThreadId}:DoSomethingBeforeAsync...");
+
+            AsyncCallback addCallBack = (IAsyncResult ia) =>
+            {
+                AsyncResult ar = (AsyncResult)ia;
+                int result = ((Func<int, int, int>)ar.AsyncDelegate).EndInvoke(ia);
+                Console.WriteLine($"当前执行的新线程，线程ID:{Thread.CurrentThread.ManagedThreadId},异步操作的结果:{result}");
+                string state = (string)ia.AsyncState;//使用IAsyncResult对象的AsyncState属性获取BeginInvoke的最后一个参数
+                Console.WriteLine($"当前执行的新线程，线程ID:{Thread.CurrentThread.ManagedThreadId},BeginInvoke的最后一个参数:{state}");//state这里是“shanzm”
+            };
+
+            IAsyncResult iar = operateAdd.BeginInvoke(1, 2, addCallBack, "shanzm");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine($"当前执行主线程，线程ID:{Thread.CurrentThread.ManagedThreadId}:...");
+            }
+
+            Console.ReadKey();
+        }
+
     }
 }
