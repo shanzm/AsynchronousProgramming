9c9fa158f94238d1fffee2a202d1590dac230ac3
shanzm <2351144690@qq.com>
2020-02-15 23:57:56 +0800

    2020年2月15日 23:57:40- Update：取消并行查询

--- 004并行Linq查询/Program.cs
+++ 004并行Linq查询/Program.cs
@@ -6,6 +6,25 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+#region 说明
+//System.Linq名称空间中有一个`ParallelEnumerable`类，该类中的方法可以分解Linq查询的工作，使其分布在多个线程上,即实现并行查询。
+
+//为并行运行而设计的LINQ查询称为** PLINQ查询**。
+
+//下面让我们先简单的理一理：
+
+//首先我们都知道`Enumerable`类为`IEnumberable<T>`接口扩展了一系列的静态方法。（就是我们使用Linq方法语法的中用的哪些常用的静态方法，自行F12）
+
+//正如MSDN中所说：“ParallelEnumberable是Enumberable的并行等效项”，`ParallelEnumberable`类则是`Enumerable`类的并行版本，
+
+//F12查看定义可以看到`ParallelEnumerable`类中几乎所有的方法都是对`ParallelQuery<TSource>`接口的扩展，
+
+//但是，在`ParallelEnumberable`类有一个重要的例外，`AsParallel()` 方法还对`IEnumerable<T>`接口的扩展，并且返回的是一个`ParallelQuery<TSource>`类型的对象，
+
+//所以呢？凡是实现类IEnumberable<T>集合可以通过调用静态方法`AsParallel()`,返回一个ParallelQuery<TSource> 类型的对象，之后就可以使用ParallelEnumerable类中的异步版本的静态查询方法了！
+
+#endregion
+
 namespace _004并行Linq查询
 {
     class Program
@@ -15,7 +34,6 @@ namespace _004并行Linq查询
             //PLinq1();
             PLinqCancel();
         }
-
 
         //示例1：简单的演示使用AsParallel()，并与正茬查询对比
         private static void PLinq1()
@@ -33,6 +51,7 @@ namespace _004并行Linq查询
             Console.WriteLine($"顺序查询，运行时间：{sw.ElapsedMilliseconds}毫秒,可以整除3的个数:{modThreeIsZero1.Count()}");
 
             //使用AsParallel()实现并行查询
+            //AsParallel()方法返回ParallelQuery&lt;TSourc>类型对象。因为返回的类型，所以编译器选择的Select()、Where()等方法是ParallelEnumerable.Where()，而不是Enumerable.Where()。
             sw.Restart();
             int[] modThreeIsZero2 = intArray.AsParallel().Select(n => n).Where(n => n % 3 == 0).OrderByDescending(n => n).ToArray();
             sw.Stop();
@@ -57,19 +76,26 @@ namespace _004并行Linq查询
                     int[] modThreeIsZero = intArray.AsParallel().WithCancellation(ct).Select(n => n).Where(n => n % 3 == 0).OrderByDescending(n => n).ToArray();
                     return modThreeIsZero;
                 }
-                catch (OperationCanceledException ex)
+                catch (OperationCanceledException ex)//一旦PLinq中取消查询就会触发OperationCanceledException异常
                 {
-
                     Console.WriteLine(ex.Message);//注意：Message的内容就是：已取消该操作
                     return null;
                 }
             }
            );
+            Console.WriteLine("取消PLinq?Y/N");
+            string input = Console.ReadLine();
+            if (input.ToLower().Equals("y"))
+            {
+                cts.Cancel();
+                Console.WriteLine("取消了PLinq！");//undone:怎么验证已经真的取消了
+            }
+            else
+            {
+                Console.WriteLine("Loading……");
+                Console.WriteLine(task.Result.Count());
+            }
 
-           // cts.Cancel();//调试的时候，通过注释掉此句和不注释掉对比
-            Console.WriteLine(task.Result ?.Count());
-            Console.WriteLine("不查了，取消PLinq");
-            Console.WriteLine();
             Console.ReadKey();
 
         }
