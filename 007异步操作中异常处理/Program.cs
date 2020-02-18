c8989976962c94535c368cb7743014931b58a23b
shanzm <2351144690@qq.com>
2020-02-18 15:18:55 +0800

    2020年2月18日 15:18:22-Create：007异步操作中异常处理

---
+++ 007异步操作中异常处理/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007异步操作中异常处理
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ThrowEx(2000, "异常信息");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+        private async static void ThrowEx(int ms, string message)
+        {
+            await Task.Delay(ms).ContinueWith(t => Console.WriteLine("hello word"));
+            throw new Exception(message);
+        }
+    }
+}
