498a8cb95bc1067eaad50023211e06f47d6a804b
shanzm <2351144690@qq.com>
2020-03-07 19:16:29 +0800

    2020年3月7日 19:15:21- Create：010MVC中的异步

---
+++ 010MVC中的异步/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _010MVC中的异步.Controllers
+{
+    public class HomeController : Controller
+    {
+        //同步操作
+        public ActionResult Index()
+        {
+            string msg = "";
+            using (StreamReader sr = new StreamReader(@"C:\Users\shanzm\Desktop\1.txt", Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    msg = sr.ReadToEnd();
+                }
+
+            }
+            return Content(msg);
+        }
+
+        //异步操作
+        public async Task<ActionResult> Index2()
+        {
+            string msg = "";
+            using (StreamReader sr = new StreamReader(@"C:\Users\shanzm\Desktop\1.txt", Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    msg = await sr.ReadToEndAsync();//使用异步版本的方法
+                }
+            }
+            return Content(msg);
+        }
+    }
+}
---
+++ 010MVC中的异步/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_010MVC中的异步.MvcApplication" Language="C#" %>
