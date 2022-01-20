293319c7e2778d5850f9e73a6bc2a535034645ab
shanzm <2351144690@qq.com>
2022-01-20 19:07:51 +0800

    2022年1月20日 19:07:36-Update：挂起线程

--- 000Thread类型/Program.cs
+++ 000Thread类型/Program.cs
@@ -5,6 +5,8 @@ namespace _000Thread类型
 {
     class Program
     {
+        //Thread 类中的一些常用方法和属性
+        //主要用于线程的创建，挂起，停止，销毁线程
         static void Main(string[] args)
         {
             //示例1：
@@ -18,18 +20,19 @@ namespace _000Thread类型
             //示例5
             //ThreadSleep();
             //示例6
-            WaitThread();
-
+            //JoinThread();
+            ThreadSuspend();
             Console.ReadKey();
         }
 
 
-        //示例1：开启一个线程执行一个方法
+        //示例1——Start()
+        //开启一个线程执行一个方法
         static void StartThread()
         {
             Console.WriteLine("这是主线程");
             Thread thread = new Thread(Do);
-            thread.Start();//start表示计划执行该线程，并不表示执行该线程，而是表示，此时已经准备好，什么时候可以执行，由CPU执行，我们无法决定
+            thread.Start();//start表示通知CLR尽快执行本线程，计划执行该线程，并不表示执行该线程，而是表示，此时已经准备好，什么时候可以执行，由CPU执行，我们无法决定
             Console.WriteLine("这是主线程");
 
 
@@ -53,8 +56,8 @@ namespace _000Thread类型
         }
 
 
-        #region Thread类中其他一些控制线程的方法
-        //示例3：Thread类中的其他一些方法
+        //示例3——Name属性
+        //通过Name属性给线程设置友好的名称
         static void ThreadDemo()
         {
             Thread thread = new Thread(() => { Console.WriteLine("test"); });
@@ -70,7 +73,7 @@ namespace _000Thread类型
         }
 
 
-        //示例4
+        //示例4——CurrentThread只读属性
         //获取当前正在执行的线程ID和name属性
         static void GetCurrentThread()
         {
@@ -84,25 +87,11 @@ namespace _000Thread类型
         }
 
 
-        //示例5
-        //线程休眠
-        static void ThreadSleep()
-        {
-            Console.WriteLine($"this is main thread");
-            Console.WriteLine(Thread.CurrentThread.ThreadState);//Running
-            Thread.Sleep(5000);
-
-            Console.WriteLine(Thread.CurrentThread.ThreadState);//Running
-            Thread thread = new Thread(() => { Console.WriteLine($"this my thread"); });
-            thread.Start();
-        }
-
-
-        //示例5
+        //示例5——Join()
         //等待线程执行结束
         //首先我们要明确线程什么时候结束，一个线程结束的条件是其构造函数传入的委托执行完毕
         //同时：一个线程一旦执行完毕就无法再次重启该线程
-        static void WaitThread()
+        static void JoinThread()
         {
             //Thread thread = new Thread(() => { Thread.Sleep(2000); Console.WriteLine("waited 2s ,this my thread"); });
             //thread.Start();
@@ -122,6 +111,48 @@ namespace _000Thread类型
             //this main thread 
 
         }
+
+        #region 挂起线程
+
+
+        //示例5——Sleep()
+        //线程休眠,线程挂起指定的时间
+        static void ThreadSleep()
+        {
+            Console.WriteLine($"this is main thread");
+            Console.WriteLine(Thread.CurrentThread.ThreadState);//Running
+            Thread.Sleep(5000);
+
+            Console.WriteLine(Thread.CurrentThread.ThreadState);//Running
+            Thread thread = new Thread(() => { Console.WriteLine($"this my thread"); });
+            thread.Start();
+        }
+
+        //示例6——Suspend()和Resume()
+        //已经弃用的方法
+        //Suspend:挂起当前线程，如果线程已经挂起，则Suspend不起作用
+        //Resume:使已经挂起的线程继续执行
+        static void ThreadSuspend()
+        {
+            int currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"this is main thread:{currentThreadId}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    Thread thread = Thread.CurrentThread;
+                    thread.Suspend();
+
+                    thread.Resume();
+
+                }
+
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
         #endregion
+
     }
 }
