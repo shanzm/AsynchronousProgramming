2a4a7160da78bda2029cddfa44e9c4b90deb6b2d
shanzm_aimer <2351144690@qq.com>
2023-02-17 17:00:26 +0800

    2023年2月17日 17:00:12-Update：测试死锁的形成

--- 00Thread类型-锁与线程安全/Program.cs
+++ 00Thread类型-锁与线程安全/Program.cs
@@ -12,8 +12,11 @@ namespace _000Thread类型_锁与线程安全
         private static void Main(string[] args)
         {
             //因为Go中有加锁，所以任何情形下都不会出现打印两次的情形，一定是打印一次
-            new Thread(Go).Start();
-            Go();
+            //new Thread(Go).Start();
+            //Go();
+
+            //测试死锁
+            TestDeadLock();
 
             Console.ReadKey();
         }
@@ -56,19 +59,46 @@ namespace _000Thread类型_锁与线程安全
 
         #region 测试死锁
 
-        private static readonly object lokcer1 = new object();
-        private static readonly object lokcer2 = new object();
+        private static readonly object locker1 = new object();
+        private static readonly object locker2 = new object();
 
-        private static void LockTooMuch(object locker1, object locker2)
+        private static void LockTooMuch(object lockerA, object lockerB)
         {
-            lock (lokcer1)
+            Console.WriteLine(Thread.CurrentThread.Name + "开始执行");
+            lock (lockerA)
             {
+                Console.WriteLine(Thread.CurrentThread.Name + "锁住lockerA（注：此时：若是线程1则锁住locker1,若是线程2锁住locker2）");
                 Thread.Sleep(1000);
-                lock (lokcer2) { };
+                lock (lockerB)
+                {
+                    Console.WriteLine("success");
+                };
             }
         }
 
-        //private new Thread(()=> )
+        //测试死锁
+        //死锁是什么?两个线程互相等待对方占用的资源就会使双方都无法继续执行，从而形成死锁。
+        private static void TestDeadLock()
+        {
+            //创建两个线程thread1和thread2,当一个线程thread1占用资源lockerA时，再去请求另外一个资源lockerB时
+            //而此时的thread2已经抢占了lockerB,并要请求lockerA的资源，但是locker又被thread1占用,也只能进行等待
+            //此时两个线程都只能一直等待对方释放资源，这就形成了死锁。
+
+            //保持对locker1对象的锁定，等待直到locker2对象被释放（此时locker2已被thread2锁定）
+            Thread thread1 = new Thread(() => LockTooMuch(locker1, locker2)); thread1.Name = "thread1";
+            //保持对locker2对象的锁定，等待直到locker1对象被释放 (此时locker1已经thread1锁定)
+            Thread thread2 = new Thread(() => LockTooMuch(locker2, locker1)); thread2.Name = "thread2";
+
+            //多线程避免死锁关键，锁定对象顺序要相同。
+            thread1.Start();
+            thread2.Start();
+        }
+
+        ///形成死锁的原因
+        ///1. 互斥条件
+
+        //解决死锁的办法
+        //1. 使用Monitor.TryEnter()方法。可以解决死锁问题，但是最会避免出现死锁的情形
 
         #endregion
     }
